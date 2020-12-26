0c14f28b0b11241efb41b5c7b128de296599cbbe
alprnkeskekoglu <alperen.keskekoglu@mediaclick.com.tr>
2020-12-26 14:40:03 +0300

    Initial Commit

---
+++ src/DawnstarServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Dawnstar;
+
+use Dawnstar\Console\Kernel;
+use Illuminate\Support\ServiceProvider;
+
+class DawnstarServiceProvider extends ServiceProvider
+{
+
+    public function boot()
+    {
+        //
+    }
+
+
+    public function register()
+    {
+        //
+    }
+
+}
