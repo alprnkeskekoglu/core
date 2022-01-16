6033a834b4f7db6b3ceb824a071e6da6ed5cb33e
alprnkeskekoglu <alperen.keskekoglu@mediaclick.com.tr>
2022-01-16 15:16:13 +0300

    sitemapxml added

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -2,6 +2,8 @@
 
 use Dawnstar\Core\Http\Controllers\WebController;
 use Dawnstar\Core\Http\Controllers\FormMessageController;
+use Dawnstar\Core\Http\Controllers\SitemapXmlController;
 
+Route::get('/sitemap.xml', [SitemapXmlController::class, 'index'])->name('sitemap_xml');
 Route::post('/form/{form}', [FormMessageController::class, 'store'])->name('form_store');
 Route::fallback([WebController::class, 'index']);
