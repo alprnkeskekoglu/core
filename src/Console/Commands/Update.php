4718c4277bce3d33a993def5372863d79f0e297f
alprnkeskekoglu <alperen.keskekoglu@mediaclick.com.tr>
2022-01-16 14:31:52 +0300

    small fixes

--- src/Console/Commands/Update.php
+++ src/Console/Commands/Update.php
@@ -23,27 +23,9 @@ class Update extends Command
 
     public function handle()
     {
-        File::deleteDirectory(public_path('vendor/dawnstar'));
-        $this->info(public_path('vendor') . " Deleted !!" . PHP_EOL);
-        $this->info('**It may take longer to publish, wait a few seconds.' . PHP_EOL);
-        Artisan::call('vendor:publish', ['--tag' => ['dawnstar-core-assets', 'dawnstar-module-builder-assets', 'dawnstar-media-manager-assets']]);
-        $this->info(Artisan::output());
-
-        if($this->option('fresh')) {
-            Artisan::call('migrate:fresh', ['--force' => true]);
-            $this->info(Artisan::output());
-        } else {
-            Artisan::call('migrate', ['--force' => true]);
-            $this->info(Artisan::output());
-        }
-
-        if($this->option('seed')) {
-            $seeder = new DatabaseSeeder();
-            $seeder->call([
-                LanguageSeeder::class
-            ]);
-            $this->info("Database seed completed!" . PHP_EOL);
-        }
+        $this->vendorPublish();
+        $this->migrate();
+        $this->seed();
 
         Artisan::call('optimize:clear');
         $this->info(Artisan::output());
@@ -52,4 +34,35 @@ class Update extends Command
         $this->info(" Update Completed " . PHP_EOL);
         $this->info("------------------" . PHP_EOL);
     }
+
+    private function vendorPublish()
+    {
+        File::deleteDirectory(public_path('vendor'));
+
+        $this->info(public_path('vendor') . " Deleted !!" . PHP_EOL);
+        $this->info('**It may take longer to publish, wait a few seconds.' . PHP_EOL);
+        Artisan::call('vendor:publish', ['--tag' => ['dawnstar-core-assets', 'dawnstar-module-builder-assets', 'dawnstar-media-manager-assets']]);
+        $this->info(Artisan::output());
+    }
+
+    private function migrate()
+    {
+        if($this->option('fresh')) {
+            Artisan::call('migrate:fresh', ['--force' => true]);
+            $this->info(Artisan::output());
+        } else {
+            Artisan::call('migrate', ['--force' => true]);
+            $this->info(Artisan::output());
+        }
+    }
+
+    private function seed()
+    {
+        if($this->option('seed')) {
+            $seeder = new DatabaseSeeder();
+            $seeder->call([LanguageSeeder::class]);
+            $this->info("Database seed completed!" . PHP_EOL);
+        }
+    }
+
 }
